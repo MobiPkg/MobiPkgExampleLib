ffa78402dcedd092da6d783969a56959ed99b7f0
cjl_macbook <cjl_spy@163.com>
2023-04-20 21:13:26 +0800

    fail: libvpx

---
+++ fail-libs/li/libvpx/1.13.0/logs/android/armeabi-v7a.sh
@@ -0,0 +1,16 @@
+ANDROID_NDK_HOME="/Volumes/Samsung-T5/sdk/android/ndk/25.2.9519653"
+CC="/Volumes/Samsung-T5/sdk/android/ndk/25.2.9519653/toolchains/llvm/prebuilt/darwin-x86_64/bin/armv7a-linux-androideabi21-clang"
+CXX="/Volumes/Samsung-T5/sdk/android/ndk/25.2.9519653/toolchains/llvm/prebuilt/darwin-x86_64/bin/armv7a-linux-androideabi21-clang++"
+AR="/Volumes/Samsung-T5/sdk/android/ndk/25.2.9519653/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-ar"
+AS="/Volumes/Samsung-T5/sdk/android/ndk/25.2.9519653/toolchains/llvm/prebuilt/darwin-x86_64/bin/armv7a-linux-androideabi21-clang"
+RANLIB="/Volumes/Samsung-T5/sdk/android/ndk/25.2.9519653/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-ranlib"
+STRIP="/Volumes/Samsung-T5/sdk/android/ndk/25.2.9519653/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-strip"
+NM="/Volumes/Samsung-T5/sdk/android/ndk/25.2.9519653/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-nm"
+LD="/Volumes/Samsung-T5/sdk/android/ndk/25.2.9519653/toolchains/llvm/prebuilt/darwin-x86_64/bin/ld.lld"
+HOST="arm-linux-androideabi"
+CFLAGS="-fPIC -O2 -Wall"
+CXXFLAGS="-fPIC -O2 -Wall"
+cd /Volumes/Samsung-T5/code/MobiPkg/Compile/logs/libs/libvpx/source/libvpx/libvpx-1.13.0
+./configure --prefix=/Volumes/Samsung-T5/code/MobiPkg/Compile/logs/libs/libvpx/install/android/armeabi-v7a --exec-prefix /Volumes/Samsung-T5/code/MobiPkg/Compile/logs/libs/libvpx/install/android/armeabi-v7a --host arm-linux-androideabi 
+cd /Volumes/Samsung-T5/code/MobiPkg/Compile/logs/libs/libvpx/source/libvpx/libvpx-1.13.0
+/Volumes/Samsung-T5/code/MobiPkg/Compile/logs/libs/libvpx/source/libvpx/libvpx-1.13.0/configure --help
